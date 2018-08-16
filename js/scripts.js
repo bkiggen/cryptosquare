88266f668c63b50524e72e9f539d4939e36c86ee
austin Cummings and Ben Kiggen <austin@>
2018-08-16 13:58:52 -0700

    translate and present scrambled sentence without spacing

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,36 @@ $(document).ready(function(){
 
     var inputSentence = $("#original-text").val();
     inputSentence = inputSentence.toLowerCase().replace(/[^A-z0-9]/g, "");
+    var sentence = inputSentence.split("");
     var area = inputSentence.length;
-    side = Math.ceil(Math.sqrt(area));
-    console.log(side);
+    var side = Math.ceil(Math.sqrt(area)); //array length
+    var cryptoMatrix = [];
+    var outputSentence = [];
+
+    var i = 0;
+
+    for(var r = 0; r < side; r++){
+          for(var c = 0; c < side; c++){
+            cryptoMatrix[[r,c]] = inputSentence[i];
+            i++;
+          }
+        }
+
+    for(var r = 0; r < side; r++){
+          for(var c = 0; c < side; c++){
+            outputSentence.push(cryptoMatrix[[c,r]]);
+            i++;
+          }
+        };
+
+      outputSentence.toString();
+      // outputSentence.replace(/(.{5})/g," ");
+
+      console.log(outputSentence);
+
+
+
+
+
   });
 });
